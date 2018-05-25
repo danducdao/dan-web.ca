5996c52a3f5c11e158e42f017c4018cccc9eb080
Dan <dan-duc.dao@hec.ca>
2018-05-25 13:04:17 -0700

    vue.js conditions

--- vue.js/src/components/Home.vue
+++ vue.js/src/components/Home.vue
@@ -4,7 +4,39 @@
      <p>Titre : {{job|}}</p>
      <p>{{ getDate()}}</p>
      <p><a v-bind:href="website">www.indeed.com</a></p>
-     <p v-hml="websiteTag"></p>
+     <p v-html="websiteTag"></p>
+     <hr>
+     <ul>
+        <li v-for="language in languages">{{language}}</li>
+     </ul>
+     <hr>
+     <ul>
+        <li v-for="(pers,index) in persons">{{++index}} . {{pers.name}} - {{pers.age}} ans</li>
+     </ul>
+     <hr>
+     <table border="1">
+        <thead>
+           <td>No.</td>
+           <td>Id</td>
+           <td>Titre</td>
+        </thead>
+        <tbody>
+         <template v-for="(job,index) in jobs">
+            <tr>
+                <td>{{++index}}</td>
+                <td>{{job.id}}</td>
+                <td>{{job.title}}</td>
+            </tr>
+        </template>
+         </tbody>
+      </table>
+      <hr>
+      <template v-for="job in jobs">
+          <ul>
+             <li v-for="(val,key) in job">{{key}} - {{val}}</li>
+          </ul>
+      </template>
+      <hr>
 
   </div>
 </template>
@@ -17,7 +49,21 @@ export default {
       name: 'Jonh Doe',
       job: 'Programmeur',
       website : 'http://www.indeed.com',
-      websiteTag : '<a href="http://exemple.com">Exemple</a>'
+      websiteTag : '<a href="http://exemple.com">Exemple</a>',
+      languages:['PHP','C#','JavaScript','Java','C++','Python'],
+      persons:[
+                {name:'Jonh',age:34},
+                {name:'Mary',age:45},
+                {name:'Mario',age:23},
+                {name:'Steve',age:60}
+              ],
+      jobs:[
+              {id:1,title:'Programmeur-Analyste'},
+              {id:2,title:'Représentant de vente'},
+              {id:3,title:'Commis de bureau'},
+              {id:4,title:'Technicien en informatique'},
+              {id:5,title:'Commis comptable'}
+            ]
     }
   },
   methods:{

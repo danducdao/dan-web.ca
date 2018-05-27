108030effe91c9eb70df1516f3d7463408866880
Dan <dan-duc.dao@hec.ca>
2018-05-27 13:16:01 -0700

    Mon premier program d'Angular 6

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstProgramComponent } from './first-program/first-program.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstProgramComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/src/app/first-program/first-program.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-program',
+  template: `<h2>Bienvenue à {{ name }}</h2>
+             <h2>{{ 2 + 2}}</h2>
+             <h2>{{ "Bienvenue à" + name}}</h2>
+             <h2>{{name.length}}</h2>
+             <h2>{{name.toUpperCase()}}</h2>
+             <h2>{{ doIt() }}</h2>
+             <h2>{{ Url }}</h2>
+             <p><input type="text" value="Mon premier program"></p>
+             `,
+  styleUrls: []
+})
+export class FirstProgramComponent implements OnInit {
+
+  public name = "Angular 6";
+  public Url = window.location.href;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  doIt(){
+      return "Mon premier program Angular 6";
+  }
+}

1b2ec9eab00454dfe7b15bd221054125e1cd42df
Dan <dan-duc.dao@hec.ca>
2018-05-27 13:28:12 -0700

    angular data binding

--- angular/src/app/first-program/first-program.component.ts
+++ angular/src/app/first-program/first-program.component.ts
@@ -9,14 +9,22 @@ import { Component, OnInit } from '@angular/core';
              <h2>{{name.toUpperCase()}}</h2>
              <h2>{{ doIt() }}</h2>
              <h2>{{ Url }}</h2>
-             <p><input type="text" value="Mon premier program"></p>
-             `,
+             <p><input type="text" [disabled]="isDisabled" value="Mon premier program"></p>
+             <p><input type="text" [id]="myId" [value]="myBinding"></p>
+             <p><input type="text" id="{{myId2}}" bind-value="myBinding2"></p>
+             <p><input type="text" id="{{myId2}}" bind-value="myBinding2"></p>`,
   styleUrls: []
 })
 export class FirstProgramComponent implements OnInit {
 
   public name = "Angular 6";
   public Url = window.location.href;
+  public myBinding = "Data Binding";
+  public myId = "Mon Id";
+  public myBinding2 = "Data Binding 2";
+  public myId2 = "Mon Id 2";
+  public isDisabled = true;
+
   constructor() { }
 
   ngOnInit() {

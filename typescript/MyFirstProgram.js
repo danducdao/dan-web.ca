35375e1dd6bcfb0763eb954736bbc2de3a36c5f3
Dan <dan-duc.dao@hec.ca>
2018-05-28 13:24:39 -0700

    Mon premier program TypeScript

---
+++ typescript/MyFirstProgram.js
@@ -0,0 +1,16 @@
+var a;
+var b;
+var c;
+a = 2;
+b = true;
+c = 'Mon premier program';
+console.log(a);
+console.log(b);
+console.log(c);
+//Array
+var myArray;
+myArray = [1, 2, 3, 4, 5];
+//Tuple
+var myTuple = [1, true];
+var myTuple2;
+myTuple2 = [2, false];
---
+++ typescript/MyFirstProgram.ts
@@ -0,0 +1,20 @@
+var a: number;
+var b: boolean;
+var c:string;
+
+a = 2;
+b = true;
+c = 'Mon premier program';
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Array
+var myArray : number[];
+myArray = [1,2,3,4,5];
+
+//Tuple
+var myTuple : [number,boolean] = [1 , true];
+var myTuple2 : [number,boolean];
+myTuple2 = [2, false];

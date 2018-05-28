07655b7181980eaa39f7d17fae19558fec04d6fe
Dan <dan-duc.dao@hec.ca>
2018-05-28 14:46:28 -0700

    TypeScript Classes

--- typescript/MyClasses.ts
+++ typescript/MyClasses.ts
@@ -1,12 +1,35 @@
+/*
+Program : Mes premières classes
+Écrit par : Dan Duc Dao
+*/
+
+//Classe Person
 class Person{
     Nom : string;
     PreNom : string;
+    constructor(){}
+    getNomComplete():string{
+        return this.PreNom + " " + this.Nom;
+    }
+}
+let aPerson : Person = new Person();
+aPerson.Nom = "Doe";
+aPerson.PreNom = "Jonh";
+var nomComplet = aPerson.getNomComplete();
+console.log(nomComplet);
+
+//Classe Person2
+class Person2{
+    Nom : string;
+    PreNom : string;
+    constructor(nom:string,prenom:string){
+       this.Nom = nom;
+       this.PreNom = prenom;
+    }
     getNomComplete():string{
         return this.Nom + " " + this.PreNom;
     }
 }
-let aPerson : Person = new Person();
-aPerson.Nom = "Jonh";
-aPerson.PreNom = "Doe";
-var nomComplet = aPerson.getNomComplete();
-console.log(nomComplet);
+
+let aPerson2 : Person = new Person2("Charles Robert","Darwin");
+console.log(aPerson2.getNomComplete());

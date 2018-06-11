4617a71810504bf91e044e2462d12418eb49061d
Dan <dan-duc.dao@hec.ca>
2018-06-11 05:15:18 -0700

    mettre des commentaires

--- mongodb/test/updating_test.js
+++ mongodb/test/updating_test.js
@@ -1,17 +1,14 @@
 /*
-* Program : Mettre à jour la la collection personne
+* Program : Mettre à jour les données de la collection personne
 * Écrit par : Dan Duc Dao
 */
 
 const mocha = require('mocha');
 const assert = require('assert');
 const Personne = require('../model/personne');
-const Categorie = require('../model/categorie');
-const Utilisateur = require('../model/utilisateur');
-const Produit = require('../model/produit');
 
-describe('Sauvegarder de données dans les collections',function(){
-      it('Sauvegarder de données dans collection personne', function(done){
+describe('Mettre à jour les données de la collection personne',function(){
+      it('Mettre à jour les données de la collection personne', function(done){
             Personne.findOneAndUpdate({nom:'Jonh Doe'},{nom:'Jonh-Doe'}).then(function(){
                  Personne.findOne({nom:"Jonh-Doe"}).then(function(result){
                       assert(result.nom === "Jonh-Doe");

bea9af51e0b60817346c99ecd78e5819b17231dd
Dan <dan-duc.dao@hec.ca>
2018-06-11 10:19:06 -0700

    ajouter nouvel requete sur la collection produit

---
+++ mongodb/test/query_test.js
@@ -0,0 +1,27 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const Produit = require('../model/produit');
+
+describe('Trouver les produits qui coûtent $18.00',function(){
+      it('Trouver les produits qui coûtent $18.00', function(done){
+              Produit.find({prix :{ $eq: 18 }}).then(function(result){
+                  console.log('\r');
+                  result.forEach(function(produit){
+                      console.log(produit.nom + " coûte $" + produit.prix.toFixed(2) + '\n');
+                  })
+                  console.log('\r');
+                  done();
+              })
+        });
+
+        it('Trouver les produits qui coûtent plus que $18.00', function(done){
+                Produit.find({prix : { $gt: 18 }}).then(function(result){
+                    console.log('\r');
+                    result.forEach(function(produit){
+                        console.log(produit.nom + " coûte $" + produit.prix.toFixed(2) + '\n');
+                    })
+                    console.log('\r');
+                    done();
+                })
+          });
+  });

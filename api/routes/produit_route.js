4f7c2d5ff53245b3bab29757fd98cede12e1c542
Dan <dan-duc.dao@hec.ca>
2018-06-12 11:04:00 -0700

    set up body parse et mongoose

--- api/routes/produit_route.js
+++ api/routes/produit_route.js
@@ -5,13 +5,16 @@
 
 const express = require('express');
 const router = express.Router();
+const Produit = require('../model/produit');
 
-router.get('/produit',function(req,res){
-     res.send({type:"GET PRODUIT"});
+router.get('/produit/:prix',function(req,res){
+    Produit.find({}).then(function(result){
+           res.send(result);
+    });
 });
 
 router.post('/produit',function(req,res){
-     res.send({type:"POST PRODUIT"});
+    res.send({type:"POST PRODUIT"});
 });
 
 router.put('/produit/:id',function(req,res){

0ac66f3f5cd017ccab4436bbccc370efa3efe3d3
Dan <dan-duc.dao@hec.ca>
2018-06-29 13:33:25 -0700

    optimiser la page home de l'app angular

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -5,96 +5,76 @@ Program : Youtube tutorial
 
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-
+import * as tuto from 'src/inc/tutoriel';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [`
-      #main .title{
-         text-align:left;
-      }
-      #previous{
-         float:left;
-      }
-      #next{
-         float:right;
-      }
-    `]
+              #main .title{
+                text-align:left;
+              }
+              #previous{
+                  float:left;
+              }
+              #next{
+                  float:right;
+              }
+              .titre{
+                  min-height:50px;
+                  padding:10px;
+                  font-weight:bold;
+                  font-size:20px;
+                  text-align:center;
+              }
+          `]
 })
 export class HomeComponent implements OnInit {
 
   public safeURL:any;
-  public isDisabled = false;
-  private videosURLs:string[];
+  public titre:string;
+  public count:number;
+  private videosURLs:any = new Object();
+  private videosTitles:any = new Object();
   private i:number = 0;
   private btnNext:HTMLElement;
   private btnPrevious:HTMLElement;
 
-  constructor(private _sanitizer: DomSanitizer) {
-        this.videosURLs = [
-           "https://www.youtube.com/embed/0eWrpsCLMJQ",
-           "https://www.youtube.com/embed/FVPGJHinItI",
-           "https://www.youtube.com/embed/mDoHtD1hI3I",
-           "https://www.youtube.com/embed/16rQyEQtpyQ",
-           "https://www.youtube.com/embed/2a6OfacW_-I",
-           "https://www.youtube.com/embed/N8FBmB2jme8",
-           "https://www.youtube.com/embed/Y6OP-lPJxgs",
-           "https://www.youtube.com/embed/q256X6-u9Q8",
-           "https://www.youtube.com/embed/ZfIc1_oj7uM",
-           "https://www.youtube.com/embed/Oo0-r_YhoJs",
-           "https://www.youtube.com/embed/DOWwWsbG1Sw",
-           "https://www.youtube.com/embed/nWst87nQmZQ",
-           "https://www.youtube.com/embed/WiDn2y1Ktws",
-           "https://www.youtube.com/embed/Du3p6QYGs3A",
-           "https://www.youtube.com/embed/BGy8DdGw_WE",
-           "https://www.youtube.com/embed/y8lwG8IM82k",
-           "https://www.youtube.com/embed/pwuGBvOPFYI",
-           "https://www.youtube.com/embed/OFPIGlxunL0",
-           "https://www.youtube.com/embed/69VeYoKzL6I",
-           "https://www.youtube.com/embed/vj0kUzKOgGQ",
-           "https://www.youtube.com/embed/LmIsbzt-S_E",
-           "https://www.youtube.com/embed/TmTGQiLBS5A",
-           "https://www.youtube.com/embed/Nehk4tBxD4o",
-           "https://www.youtube.com/embed/QC6wRxXT26I",
-           "https://www.youtube.com/embed/qh5nHv-4aw0",
-           "https://www.youtube.com/embed/KRV9jZwl0Ig",
-           "https://www.youtube.com/embed/gnTFkl2AF-w",
-           "https://www.youtube.com/embed/qktmk-7Kot8",
-           "https://www.youtube.com/embed/ZoeZxpfTCXk",
-           "https://www.youtube.com/embed/RkV_LozGP3w",
-           "https://www.youtube.com/embed/ScaKGrW5s0I"
-        ];
-  }
+  constructor(private _sanitizer: DomSanitizer) {}
+
   ngOnInit() {
-      this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videosURLs[this.i]);
+      this.setVideo(
+                      tuto.getCodeEvolutionTuto()[this.i].url,
+                      tuto.getCodeEvolutionTuto()[this.i].titre,
+                      this.i
+                   );
       this.btnNext = document.getElementById("next")
       this.btnPrevious = document.getElementById("previous")
   }
-
+  setVideo(url,titre,i){
+      this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(url);
+      this.titre = titre;
+      this.count = ++i;
+  }
   next(){
-       if(this.i < this.videosURLs.length - 1)
+       var i = 0;
+       if(this.i < tuto.getCodeEvolutionTuto().length - 1)
        {
-         if(this.btnPrevious.getAttribute("disabled"))
-         {
-           this.btnPrevious.removeAttribute("disabled");
-         }
-          this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videosURLs[++this.i]);
+          var i = ++this.i;
        }else{
-          this.btnNext.setAttribute("disabled", "disabled");
+           this.i = 0;
+           i = this.i;
        }
+       this.setVideo(tuto.getCodeEvolutionTuto()[i].url,tuto.getCodeEvolutionTuto()[i].titre,i);
   }
-
   previous(){
-
-     if(this.i > 0)
-     {
-        if(this.btnNext.getAttribute("disabled"))
-        {
-           this.btnNext.removeAttribute("disabled");
-        }
-        this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videosURLs[--this.i]);
-     }else{
-         this.btnPrevious.setAttribute("disabled", "disabled");
-     }
-  }
+      var i = 0
+      if(this.i > 0)
+      {
+          i = --this.i;
+      }else{
+          this.i = tuto.getCodeEvolutionTuto().length - 1;
+          i = this.i;
+      }
+      this.setVideo(tuto.getCodeEvolutionTuto()[i].url,tuto.getCodeEvolutionTuto()[i].titre,i);
+   }
 }
---
+++ angular/src/inc/tutoriel.js
@@ -0,0 +1,129 @@
+export var getCodeEvolutionTuto = function()
+{
+  return [
+            {
+               titre:"Introduction",
+               url: "https://www.youtube.com/embed/0eWrpsCLMJQ"
+            },
+            {
+               titre:"Getting Started",
+               url:"https://www.youtube.com/embed/FVPGJHinItI"
+            },
+            {
+                titre:"Hello World App",
+                url:"https://www.youtube.com/embed/mDoHtD1hI3I",
+            },
+            {
+                titre:"Components",
+                url:"https://www.youtube.com/embed/16rQyEQtpyQ"
+            },
+            {
+                titre:"Interpolation",
+                url:"https://www.youtube.com/embed/2a6OfacW_-I"
+            },
+            {
+                titre:"Property Binding",
+                url:"https://www.youtube.com/embed/N8FBmB2jme8"
+            },
+            {
+                titre:"Class Binding",
+                url:"https://www.youtube.com/embed/Y6OP-lPJxgs"
+            },
+            {
+                titre:"Style Binding",
+                url:"https://www.youtube.com/embed/q256X6-u9Q8"
+            },
+            {
+                titre:"Event Binding",
+                url:"https://www.youtube.com/embed/ZfIc1_oj7uM"
+            },
+            {
+                titre:"Template Reference Variables",
+                url:"https://www.youtube.com/embed/Oo0-r_YhoJs"
+            },
+            {
+                titre:"Two Way Binding",
+                url:"https://www.youtube.com/embed/DOWwWsbG1Sw"
+            },
+            {
+                titre:"ngIf Directive",
+                url:"https://www.youtube.com/embed/nWst87nQmZQ"
+            },
+            {
+                 titre:"ngSwitch Directive",
+                 url:"https://www.youtube.com/embed/WiDn2y1Ktws"
+            },
+            {
+                 titre:"ngFor Directive",
+                 url:"https://www.youtube.com/embed/Du3p6QYGs3A"
+            },
+            {
+                titre:"Component Interaction",
+                url:"https://www.youtube.com/embed/BGy8DdGw_WE"
+            },
+            {
+                 titre:"Pipes",
+                 url:"https://www.youtube.com/embed/y8lwG8IM82k"
+            },
+            {
+                 titre:"Services",
+                 url:"https://www.youtube.com/embed/pwuGBvOPFYI"
+            },
+            {
+                titre:"Dependency Injection",
+                url:"https://www.youtube.com/embed/OFPIGlxunL0"
+            },
+            {
+               titre:"Using a Service",
+               url:"https://www.youtube.com/embed/69VeYoKzL6I"
+            },
+            {
+                titre:"HTTP and Observables",
+                url:"https://www.youtube.com/embed/vj0kUzKOgGQ"
+            },
+            {
+                titre:"Fetch Data Using HTTP",
+                url:"https://www.youtube.com/embed/LmIsbzt-S_E"
+            },
+            {
+                titre:"HTTP Error Handling",
+                url:"https://www.youtube.com/embed/TmTGQiLBS5A"
+            },
+            {
+                titre:"Routing and Navigation",
+                url:"https://www.youtube.com/embed/Nehk4tBxD4o"
+            },
+            {
+                titre:"Wildcard Route and Redirecting Routes",
+                url:"https://www.youtube.com/embed/QC6wRxXT26I"
+            },
+            {
+                titre:"Route Parameters",
+                url:"https://www.youtube.com/embed/qh5nHv-4aw0"
+            },
+            {
+                titre:"paramMap Observable",
+                url:"https://www.youtube.com/embed/KRV9jZwl0Ig"
+            },
+            {
+                titre:"Optional Route Parameters",
+                url:"https://www.youtube.com/embed/gnTFkl2AF-w"
+            },
+            {
+                titre:"Relative Navigation",
+                url:"https://www.youtube.com/embed/qktmk-7Kot8"
+            },
+            {
+                titre:"Child Routes",
+                url:"https://www.youtube.com/embed/ZoeZxpfTCXk"
+            },
+            {
+                titre:"Angular 6 New Features and Changes",
+                url:"https://www.youtube.com/embed/RkV_LozGP3w"
+            },
+            {
+                titre:"Updating your app to angular 6",
+                url:"https://www.youtube.com/embed/ScaKGrW5s0I"
+            }
+      ];
+}

009199bb97137b88ecc15e86736b30b06431de31
Dan <dan-duc.dao@hec.ca>
2018-07-07 10:56:33 -0700

    ajouter un nouvel tutoriel Python 3 for beginners

--- vue.js/src/components/home.vue
+++ vue.js/src/components/home.vue
@@ -11,36 +11,36 @@
                     <div class="row">
                         <div class="col-lg-6 border-right">
                                 <label class="col-sm-2 control-label">Vidéo : </label>
-                                <select v-model="Vuejs2.itemSelected" v-on:change="Vuejs2.selectedItem()">
-                                        <option v-for="(item,index) in Vuejs2.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
+                                <select v-model="tutoriels.Vuejs2.itemSelected" v-on:change="tutoriels.Vuejs2.selectedItem()">
+                                        <option v-for="(item,index) in tutoriels.Vuejs2.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
                                 </select>
-                                <h2 class="title"><strong>Vue JS 2 Tutorial</strong></h2>
+                                <h2 class="title"><strong>Vue JS 2</strong></h2>
                                 <p>
-                                        <iframe width="580" height="315" v-bind:src="Vuejs2.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        <iframe width="580" height="315" v-bind:src="tutoriels.Vuejs2.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                                 </p>
                                 <p class="titre">
-                                    <span>Tutorial #{{Vuejs2.count}} - {{ Vuejs2.titre }}</span>
+                                    <span>{{tutoriels.Vuejs2.count}} - {{ tutoriels.Vuejs2.titre }}</span>
                                 </p>
                                 <p>
-                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="Vuejs2.previous()">Précédent</button>
-                                    <button type="button" class="btn btn-primary" id="next" v-on:click="Vuejs2.next()">Suivant</button>
+                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="tutoriels.Vuejs2.previous()">Précédent</button>
+                                    <button type="button" class="btn btn-primary" id="next" v-on:click="tutoriels.Vuejs2.next()">Suivant</button>
                                 </p>
                         </div>
                         <div class="col-lg-6">
                                 <label class="col-sm-2 control-label">Vidéo : </label>
-                                <select v-model="TypeScriptBasic.itemSelected" v-on:change="TypeScriptBasic.selectedItem()">
-                                        <option v-for="(item,index) in TypeScriptBasic.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
+                                <select v-model="tutoriels.TypeScriptBasic.itemSelected" v-on:change="tutoriels.TypeScriptBasic.selectedItem()">
+                                        <option v-for="(item,index) in tutoriels.TypeScriptBasic.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
                                 </select>
-                                <h3 class="title"><strong>TypeScript Basics</strong></h3>
+                                <h2 class="title"><strong>TypeScript Basics</strong></h2>
                                 <p>
-                                        <iframe width="580" height="315" v-bind:src="TypeScriptBasic.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        <iframe width="580" height="315" v-bind:src="tutoriels.TypeScriptBasic.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                                 </p>
                                 <p class="titre">
-                                    <span>Tutorial #{{TypeScriptBasic.count}} - {{ TypeScriptBasic.titre }}</span>
+                                    <span>{{tutoriels.TypeScriptBasic.count}} - {{ tutoriels.TypeScriptBasic.titre }}</span>
                                 </p>
                                 <p>
-                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="TypeScriptBasic.previous()">Précédent</button>
-                                    <button type="button" class="btn btn-primary" id="next" v-on:click="TypeScriptBasic.next()">Suivant</button>
+                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="tutoriels.TypeScriptBasic.previous()">Précédent</button>
+                                    <button type="button" class="btn btn-primary" id="next" v-on:click="tutoriels.TypeScriptBasic.next()">Suivant</button>
                                 </p>
                         </div>
                     </div>
@@ -51,25 +51,42 @@
                     <div class="panel-tools">
                         <a class="showhide"><i class="fa fa-chevron-up"></i></a>
                     </div>
-                    Node JS
+                    Node JS, Python 3 for beginners
                 </div>
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-6 border-right">
                                 <label class="col-sm-2 control-label">Vidéo : </label>
-                                <select v-model="Nodejs.itemSelected" v-on:change="Nodejs.selectedItem()">
-                                        <option v-for="(item,index) in Nodejs.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
+                                <select v-model="tutoriels.Nodejs.itemSelected" v-on:change="tutoriels.Nodejs.selectedItem()">
+                                        <option v-for="(item,index) in tutoriels.Nodejs.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
                                 </select>
                                 <h2 class="title"><strong>Node JS</strong></h2>
                                 <p>
-                                        <iframe width="580" height="315" v-bind:src="Nodejs.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                        <iframe width="580" height="315" v-bind:src="tutoriels.Nodejs.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                                 </p>
                                 <p class="titre">
-                                    <span>Tutorial #{{Nodejs.count}} - {{ Nodejs.titre }}</span>
+                                    <span>{{tutoriels.Nodejs.count}} - {{ tutoriels.Nodejs.titre }}</span>
                                 </p>
                                 <p>
-                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="Nodejs.previous()">Précédent</button>
-                                    <button type="button" class="btn btn-primary" id="next" v-on:click="Nodejs.next()">Suivant</button>
+                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="tutoriels.Nodejs.previous()">Précédent</button>
+                                    <button type="button" class="btn btn-primary" id="next" v-on:click="tutoriels.Nodejs.next()">Suivant</button>
+                                </p>
+                        </div>
+                        <div class="col-lg-6">
+                                <label class="col-sm-2 control-label">Vidéo : </label>
+                                <select v-model="tutoriels.Python3.itemSelected" v-on:change="tutoriels.Python3.selectedItem()">
+                                        <option v-for="(item,index) in tutoriels.Python3.listItem()" v-bind:value="item.id">{{ index + 1 }} - {{ item.titre}}</option>
+                                </select>
+                                <h2 class="title"><strong>Python 3 for beginners</strong></h2>
+                                <p>
+                                        <iframe width="580" height="315" v-bind:src="tutoriels.Python3.url" frameborder="1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                </p>
+                                <p class="titre">
+                                    <span>{{tutoriels.Python3.count}} - {{ tutoriels.Python3.titre }}</span>
+                                </p>
+                                <p>
+                                    <button type="button" class="btn btn-primary" id="previous"  v-on:click="tutoriels.Python3.previous()">Précédent</button>
+                                    <button type="button" class="btn btn-primary" id="next" v-on:click="tutoriels.Python3.next()">Suivant</button>
                                 </p>
                         </div>
                     </div>
@@ -86,17 +103,18 @@ import youtubeEmbedUrls from '../inc/tutoriel';
 export default {
         name: 'Home',
         data () {
-          return {
-                Vuejs2:"",
-                TypeScriptBasic:"",
-                Nodejs:""
-            }
+            return {
+                    tutoriels:{}
+                }
         },
         created(){
-                var i = 0;
-                this.Vuejs2 = new Tuto(youtubeEmbedUrls()[i]);
-                this.TypeScriptBasic = new Tuto(youtubeEmbedUrls()[++i]);
-                this.Nodejs = new Tuto(youtubeEmbedUrls()[++i]);      
+            var i = 0;
+            this.tutoriels = {
+                    "Vuejs2" : new Tuto(youtubeEmbedUrls()[i]),
+                    "TypeScriptBasic" : new Tuto(youtubeEmbedUrls()[++i]),
+                    "Nodejs" : new Tuto(youtubeEmbedUrls()[++i]),
+                    "Python3" : new Tuto(youtubeEmbedUrls()[++i]) 
+            };    
         }
   }
 </script>

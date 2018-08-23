d7372349a664c6ee17cff5816a52a7c71d9cb882
Dan <dan-duc.dao@hec.ca>
2018-08-22 20:25:54 -0700

    ajouter la condition else

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const shoppingCartMusicController = require("./controller/musics/shoppingCartCon
 const food = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "asiavn73",
+  password: "*****",
   database: "food"
 });
 
@@ -30,7 +30,7 @@ const food = mysql.createConnection({
 const music = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "asiavn73",
+  password: "*****",
   database: "music"
 });
 

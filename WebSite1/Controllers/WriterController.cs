9843acbfc8ace866824d52d742b692960c386a27
dan demo <dan-duc.dao@hec.ca>
2020-03-16 21:11:05 -0400

    init ASP.Net MVC

---
+++ WebSite1/Controllers/WriterController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebSite1.Controllers
+{
+    public class WriterController : Controller
+    {
+        // GET: Writer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebSite1/obj/Release/Package/PackageTmp/Scripts/writer.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+
+$("#sortWriter").on("change", function () {
+    if ($(this).val() == "") {
+        $("#result").html("");
+        return;
+    }
+
+    if ($(this).val() == "1") {
+        $.ajax({
+            method: "GET",
+            url: "http://webapi.com/writer/sortByName",
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                var table = "<table class='ui celled striped table'>";
+                table += "<tbody>";
+                response.forEach(function (writer) {
+                    table += "<tr>";
+                    table += "<td>" + writer.Name + "</td>";
+                    table += "<td>" + writer.HomeState + "</td>";
+                    table += "</tr>";
+                });
+                table += "</tbody>";
+                table += "</table>";
+                $("#result").html(table);
+            },
+            error: function (jqXHR, exception) {
+                console.log(jqXHR.status);
+                console.log(exception);
+            }
+        });
+    }   
+});
